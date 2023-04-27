421dc26fd70b8000d8737c039449de9e3e5c57fa
David Martínez Álvarez <a22davidma@ATEIS.LOCAL>
2023-04-27 09:05:25 +0200

    a

---
+++ laEstanteria/src/laestanteria/almacen.java
@@ -0,0 +1,39 @@
+package laestanteria;
+
+/**
+ *
+ * @author a22davidma
+ */
+public class almacen {
+    
+    private String nombre;
+    private String seccion;
+
+    public almacen(String nombre, String seccion) {
+        this.nombre = nombre;
+        this.seccion = seccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSeccion() {
+        return seccion;
+    }
+
+    public void setSeccion(String seccion) {
+        this.seccion = seccion;
+    }
+    
+    
+    
+    public void consultarStock(String subTipo, String marca) {
+        
+    }
+    
+}
