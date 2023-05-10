e248afadb03cf5fa8bc853334f83306a09d30afd
David Martínez Álvarez <a22davidma@ateis.local>
2023-05-10 12:42:57 +0200

    a

--- permiso_usuarios.sql
+++
@@ -1,6 +0,0 @@
-use laestanteria;
-GRANT select ON laestanteria.almacen TO 'estanteria'@'192.168.109.24';
-GRANT select ON laestanteria.pedido TO 'estanteria'@'192.168.109.24';
-GRANT select ON laestanteria.producto TO 'estanteria'@'192.168.109.24';
-GRANT select ON laestanteria.usuario TO 'estanteria'@'192.168.109.24';
-show grants for 'estanteria'@'192.168.109.06';
