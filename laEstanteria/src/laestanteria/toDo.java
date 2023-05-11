6e417492f584bd4bc592095f7e98396eafa425ad
David Martínez Álvarez <a22davidma@ATEIS.LOCAL>
2023-05-11 08:47:00 +0200

    a

---
+++ laEstanteria/src/laestanteria/toDo.java
@@ -0,0 +1,16 @@
+package laestanteria;
+
+/**
+ *
+ * @author a22davidma
+ */
+public class toDo {
+    /*Lista de to do*/
+    /*
+    -Acabar la clase dao
+    -Meter lectura y escritura de ficheros(quizas para hacer txt de pedidos)
+    -Acabar la interfaz gráfica
+    -Control de errores
+    -
+    */
+}
