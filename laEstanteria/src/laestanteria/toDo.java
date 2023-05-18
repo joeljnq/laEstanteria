8ee5e99a9d6a4184c0b8d51671ee841daef53b2b
David Martínez Álvarez <davidma.vigo@gmail.com>
2023-05-18 09:03:15 +0200

    a

--- laEstanteria/src/laestanteria/toDo.java
+++ laEstanteria/src/laestanteria/toDo.java
@@ -8,7 +8,7 @@ public class toDo {
     /*Lista de to do*/
     /*
     -Acabar la clase dao
-    -Meter lectura y escritura de ficheros(quizas para hacer txt de pedidos)
+    -Meter lectura y escritura de ficheros(quizas para hacer txt de pedidos)(acabado)
     -Acabar la interfaz gráfica(acabada)
     -Control de errores
     -Hacer extras(si sobra tiempo)
